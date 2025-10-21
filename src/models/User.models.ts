d46675fa35cd16b87a89aee93c201ae0173e8a24
Kandy-Hamisi <hkandy2000@gmail.com>
2025-10-21 23:08:13 +0300

    setup login route with token enabled

--- src/models/User.models.ts
+++ src/models/User.models.ts
@@ -1,28 +1,44 @@
 import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  profileImage?: string;
+}
+
+export interface IUserMethods {
+  comparePassword: (candidatePassword: string) => Promise<boolean>;
+}
+
+export type UserModel = mongoose.Model<IUser, {}, IUserMethods>;
+
+const userSchema = new mongoose.Schema<IUser, UserModel, IUserMethods>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default:
+        "https://img.freepik.com/free-vector/user-circles-set_78370-4704.jpg?semt=ais_hybrid&w=740&q=80",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  profileImage: {
-    type: String,
-    default:
-      "https://img.freepik.com/free-vector/user-circles-set_78370-4704.jpg?semt=ais_hybrid&w=740&q=80",
-  },
-});
+  { timestamps: true },
+);
 
 // hash the password before saving it to the database
 userSchema.pre("save", async function (next) {
@@ -34,6 +50,13 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-const User = mongoose.model("User", userSchema);
+// compare the password with the hashed password
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string,
+) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model<IUser, UserModel>("User", userSchema);
 
 export default User;
